ef40aa16028b279cbd3af7f9a24badbd90134d67
aymkin <aymkin@gmail.com>
2019-07-03 12:04:29 +0300

    done task #2 of HW 6

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,24 @@
-// Your code goes here
+const isTriangle = (a, b, c) => {
+  if (
+    a < b + c
+    & b < c + a
+    & c < b + a
+    & a > 0
+    & b > 0
+    & c > 0) {
+    if (a === b & b === c & c === a) {
+      return 'Eequivalent triangle';
+    } else if (a === b || a === c || b === c) {
+      return 'Isosceles triangle';
+    } else {
+      return 'Normal triangle';
+    }
+  } else {
+    return 'Triangle doesn’t exist';
+  }
+}
+
+console.log(isTriangle(5, 5, 5));
+console.log(isTriangle(5, 5, 6));
+console.log(isTriangle(3, 4, 5));
+console.log(isTriangle(10, 2, 7));
