643201c98a69d8e72494b5bfbafeb59176172744
aymkin <aymkin@gmail.com>
2019-07-03 12:26:16 +0300

    add prompt for user and alert the result

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,11 +1,15 @@
+const a = prompt('enter triangles length side A');
+const b = prompt('enter triangles length side B');
+const c = prompt('enter triangles length side C');
+
 const isTriangle = (a, b, c) => {
   if (
-    a < b + c
-    & b < c + a
-    & c < b + a
-    & a > 0
-    & b > 0
-    & c > 0) {
+    +a < +b + +c
+    & +b < +c + +a
+    & +c < +b + +a
+    & +a > 0
+    & +b > 0
+    & +c > 0) {
     if (a === b & b === c & c === a) {
       return 'Eequivalent triangle';
     } else if (a === b || a === c || b === c) {
@@ -22,3 +26,5 @@ console.log(isTriangle(5, 5, 5));
 console.log(isTriangle(5, 5, 6));
 console.log(isTriangle(3, 4, 5));
 console.log(isTriangle(10, 2, 7));
+
+alert(isTriangle(a, b, c));
